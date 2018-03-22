1dbcd50633a3e1bdb2bdba1ce823758c8a5d43e8
KKGanguly <kkganguly.iit.du@gmail.com>
2018-03-22 21:29:53 +0600

    updated setuptools

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
-from setuptools
-import setup, find_packages
+from setuptools import setup, find_packages
 setup(name = 'PapMinPy',
     version = '0.1a',
     description = 'a paper parsing package developed for parsing different items from academic papers in pdf',
